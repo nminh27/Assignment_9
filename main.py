d58fd69cca27a43ebf761c1ff6c0d6f69bbc976f
nminh27 <minhnn2708@gmail.com>
2025-05-07 19:09:38 +0900

    conflict resolved

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from typing import List
 
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
-    # implemented as a one-liner
     lines = open(path, 'r').read().split('\n')
     return lines
 
